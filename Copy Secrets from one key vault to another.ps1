6722b3446954c3eb83581b857f7e94a497c6c426
rsr-451 <40347409+rsr-451@users.noreply.github.com>
2025-02-10 12:58:39 +0530

    Add files via upload

---
+++ Copy Secrets from one key vault to another.ps1
@@ -0,0 +1,32 @@
+﻿#######Powershell script for copying secrets from one Azure Key Vault to another#####
+
+# Variables for the source and destination Key Vault names
+$sourceVaultName = "pot2"
+$destVaultName = "pot3"
+
+# Retrieve all secrets from the source Key Vault
+$secrets = Get-AzKeyVaultSecret -VaultName $sourceVaultName
+
+# Loop through each secret and copy it to the destination Key Vault
+foreach ($secretMetadata in $secrets) {
+    $secretName = $secretMetadata.Name
+    
+    # Retrieve the secret value from the source Key Vault using the secret name
+    $secretValue = Get-AzKeyVaultSecret -VaultName $sourceVaultName -Name $secretName -AsPlainText
+    
+    # Log the secret name and value for debugging
+    Write-Host "Secret Name: $secretName"
+    Write-Host "Secret Value: $secretValue"
+    
+    # Check if the secret value is not null
+    if ($null -ne $secretValue) {
+        # Convert the secret value to a SecureString
+        $secureSecretValue = ConvertTo-SecureString -String $secretValue -AsPlainText -Force
+        
+        # Set the secret value in the destination Key Vault
+        Set-AzKeyVaultSecret -VaultName $destVaultName -Name $secretName -SecretValue $secureSecretValue
+        Write-Host "Successfully copied secret: $secretName"
+    } else {
+        Write-Host "Secret '$secretName' has no value and will not be copied."
+    }
+}
---
+++ Key vault secrets copy.ps1
@@ -0,0 +1,22 @@
+﻿## This Script will copy the secret from source key vault to destination key vault
+## Source key vault = pot0
+## Destination key vault = pot1
+## source secret = sec1
+## destination secret = pot0secret
+Install-Module Az.KeyVault
+Connect-AzAccount
+Get-AzKeyVaultSecret -VaultName "pot0"
+$sourcesecvalue = Get-AzKeyVaultSecret -VaultName "pot0" -Name "sec1" -AsPlainText
+$secretSecure = ConvertTo-SecureString -String $sourcesecvalue -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName 'pot1' -Name 'pot0secret' -SecretValue $secretSecure
+Get-AzKeyVaultSecret -VaultName 'pot1' -Name 'pot0secret' -AsPlainText
+
+## Source key vault = pot1
+## Destination key vault = pot0
+## source secret = pot0secret
+## destination secret = sec1
+
+$sourcesecvalue = Get-AzKeyVaultSecret -VaultName "pot1" -Name "pot0secret" -AsPlainText
+$secretSecure = ConvertTo-SecureString -String $sourcesecvalue -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName 'pot0' -Name 'sec1' -SecretValue $secretSecure
+Get-AzKeyVaultSecret -VaultName 'pot0' -Name 'sec1' -AsPlainText
